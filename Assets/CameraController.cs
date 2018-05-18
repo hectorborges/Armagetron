54d8121ddeffb108cd215f63b723827db7f369bc
hectorborges <developer@hectorborges.com>
2018-05-18 07:04:40 -0500

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float followSpeed;
+    public Transform target;
+    public Vector3 offset;
+
+    Vector3 velocity;
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.transform.position + offset, ref velocity, followSpeed);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            Quaternion newRoataion = Quaternion.Euler(Vector3.up * -90);
+            newRoataion = Quaternion.LookRotation(transform.forward);
+            transform.rotation = newRoataion;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Quaternion newRoataion = Quaternion.Euler(Vector3.up * 90);
+            newRoataion = Quaternion.LookRotation(transform.forward);
+            transform.rotation = newRoataion;
+        }
+    }
+}
